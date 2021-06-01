c9685ba372e8ad4d5f273f00e0615f0af69d5fb6
Alex <alekceytr@mail.ru>
2021-06-01 23:40:04 +0800

    fist init

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    dbName: "lireddit",
+    type: "postgresql",
+    debug: !__prod__,
+    entities: [Post],
+  });
+
+  orm.em.create(Post, {title: 'my first post'})
+};
+
+main();
