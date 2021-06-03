ca1df7d60aef7dcac4583496e132c8460377b45b
Alex <alekceytr@mail.ru>
2021-06-03 23:45:32 +0800

    add mikro-orm

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import mikroOrmConfig from './mikro-orm.config';
 
 const main = async () => {
-  const orm = await MikroORM.init({
-    dbName: "lireddit",
-    type: "postgresql",
-    debug: !__prod__,
-    entities: [Post],
-  });
-
-  const post = orm.em.create(Post, {title: 'my first post'})
+  const orm = await MikroORM.init(mikroOrmConfig);
+  await orm.getMigrator().up();
+  // const post = orm.em.create(Post, { title: 'my first post' });
+  // await orm.em.persistAndFlush(post);
+  console.log('ffdd');
 };
 
-main();
+main().catch((err) => {
+  console.log(err);
+});
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,20 @@
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import path from 'path';
+
+console.log('dirname', __dirname);
+
+export default {
+  migrations: {
+    path: path.join(__dirname, '/migrations'),
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+    emit: 'ts'
+  },
+  dbName: 'lireddit',
+  user: 'postgres',
+  password: 'admin312',
+  type: 'postgresql',
+  debug: !__prod__,
+  entities: [Post],
+} as Parameters<typeof MikroORM.init>[0];
